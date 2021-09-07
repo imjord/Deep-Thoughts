a96e7bf10a8d3916bded8731edf62ba15cd716ea
imjord <dean.jay.goat@gmail.com>
2021-09-07 17:59:35 -0600

    added schemas and server for it

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+// import apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// new appollo server to pass in our schema data 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+// intergrate our appollo server with the express application as middleware
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,5 +23,7 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log where we can test our gql api 
+    console.log(` use graphql at http://localhost:${PORT}${server.graphqlPath}`)
   });
 });
